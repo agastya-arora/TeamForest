cef391b3b3bbcbf0c751197ad314f8469ca0ee3c
Agastya <aarora377@gatech.edu>
2026-07-10 19:24:33 +0530

    converted code to a function where argument is no. of vertices

--- Generating string.py
+++ Generating string.py
@@ -1,18 +1,19 @@
 import random
 import numpy as np
 
-NumOfVertices=10
-NumOfRows=random.randint(1,NumOfVertices-1) # number of rows of the block
-NumOfCol=NumOfVertices-NumOfRows # number of columns in the block
-NumOfElements=NumOfCol*NumOfRows #in the block
-print(NumOfRows)
-string=""
+def generate_string(n):
+    NumOfVertices=n
+    NumOfRows=random.randint(1,NumOfVertices-1) # number of rows of the block
+    NumOfCol=NumOfVertices-NumOfRows # number of columns in the block
+    NumOfElements=NumOfCol*NumOfRows #in the block
+    print(NumOfRows)
+    string=""
 
-for i in range(NumOfElements):
-    a=random.randint(0,1)
-    conv=str(a)
-    string+=conv
+    for i in range(NumOfElements):
+        a=random.randint(0,1)
+        conv=str(a)
+        string+=conv
     
-conv=str(NumOfRows)
-string+=conv
-print(string)
+    conv=str(NumOfRows)
+    string+=conv
+    return(string)
