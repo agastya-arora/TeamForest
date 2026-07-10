cac055e530a47e7b6c700a2fe6909332f2ee57d3
agastya-arora <aarora377@gatech.edu>
2026-07-10 05:04:58 -0700

    Update Generating string.py
    added comments and whitespaces to make the code neater

--- Generating string.py
+++ Generating string.py
@@ -1,17 +1,18 @@
 import random
 import numpy as np
-initial=random.randint(1,10)
-"multiply=random.randint(1,10)"
+
 NumOfVertices=10
-NumOfRows=random.randint(1,NumOfVertices-1)
-NumOfCol=NumOfVertices-NumOfRows
-NumOfElements=NumOfCol*NumOfRows
+NumOfRows=random.randint(1,NumOfVertices-1) # number of rows of the block
+NumOfCol=NumOfVertices-NumOfRows # number of columns in the block
+NumOfElements=NumOfCol*NumOfRows #in the block
 print(NumOfRows)
 string=""
+
 for i in range(NumOfElements):
     a=random.randint(0,1)
     conv=str(a)
     string+=conv
+    
 conv=str(NumOfRows)
 string+=conv
 print(string)
