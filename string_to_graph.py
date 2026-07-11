211d5bd8541725cbafe32dd5823b495ba0d6e345
CoraShaklee <cshaklee06@gmail.com>
2026-07-11 14:12:35 -0400

    Replace random graph creation with matrix generation
    Uses string from generate_string file to create matrix

--- string_to_graph.py
+++ string_to_graph.py
@@ -1,35 +1,30 @@
-import random
+import generate_string as gs
 
-def create_random_graph():
-    my_string = [random.randint(1, 5)]
-    for x in range(my_string[0]*(random.randint(1,5))):
-        my_string.append(random.randint(0,1))
-    return my_string
+def create_matrix(vertices):
+    string = gs.generate_string(vertices)
+    print(string)
+    LengthOfString = len(string) -1
+    n=int(string[LengthOfString])#Number of rows
+    m = int((len(string)-1)/n)#Number of columns
+    G = [[0 for _ in range(vertices)] for _ in range(vertices)] #Zero matrix
 
-my_string = create_random_graph() #Is a list not a string
-print(my_string)
-n=my_string[0]
-m = int((len(my_string)-1)/n)
-#Will give an n,m bipartite graph
-vertices = n+m
-print(vertices)
-G = [[0 for _ in range(vertices)] for _ in range(vertices)] #Adjacency matrix
+    CountStringItem = 0
+    for x in range(n):
+        CountColumn = n
+        for z in G[x][n:]:
+            G[x][CountColumn]=int(string[CountStringItem])
+            CountStringItem+=1
+            CountColumn+=1
 
-count2 = 1
-for x in range(n):
-    count1 = n
-    for z in G[x][n:]:
-        G[x][count1]=my_string[count2]
-        count2+=1
-        count1+=1
+    for i in range(n):
+        for j in range(vertices):
+            #print(G[i][j])
+            G[j][i]=G[i][j]
+    return G
 
-for i in range(n):
-    for j in range(vertices):
-        print(G[i][j])
-        G[j][i]=G[i][j]
-
-for x in G:
+for x in create_matrix(int(input('Number of Vertices: '))):
     print(x)
+
 
 
 
