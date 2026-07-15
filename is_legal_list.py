b41b5b42031e74142381f76865c75c994cf5dac2
Agastya <aarora377@gatech.edu>
2026-07-15 15:03:33 +0530

    recognized a mistake, and made some minor changes for efficiency

--- is_legal_list.py
+++ is_legal_list.py
@@ -1,13 +1,12 @@
 def is_legal_list(lst):
     print(lst)
     NumOfRows=lst.pop(0) #removes first element from the list
+    if len(lst)%NumOfRows!=0:
+        return False
     for i in lst:
         if i!=0 or i!=1:
             return False
-    if len(lst)//NumOfRows:
-        return True
-    else:
-        return False
+    return True
     
     
     
