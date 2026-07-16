7c9a418d9a977d1826b3971e013cb9c6b621d46f
xinuzo <rendy.adinata88@gmail.com>
2026-07-16 19:55:37 +0700

    add comments to find_cycles()

--- find_cycles.py
+++ find_cycles.py
@@ -11,22 +11,22 @@ def find_cycle(A):
         bool: True if a cycle exists, False otherwise.
     """
     adj_list = matrix_to_list(A)
-    n = len(adj_list)
-    visited = [False] * n
+    n = len(adj_list) # num of vertices
+    visited = [False] * n   # to mark visited vertex
 
     def dfs(u, parent):
-        visited[u] = True
+        visited[u] = True #current vertex marked as visited
         for v in adj_list[u]:
-            if not visited[v]:
-                if dfs(v, u):
-                    return True
-            elif v != parent:
-                return True
+            if not visited[v]: # if adjacent vertex has not been visited
+                if dfs(v, u): # if adjacent vertex is visited
+                    return True # cycle found 
+            elif v != parent: # if adjacent vertex is visited and not parent 
+                return True # cycle found 
         return False
 
-    for i in range(n):
+    for i in range(n): # iterate over all vertices
         if not visited[i]:
-            if dfs(i, -1):
+            if dfs(i, -1): # if cycle found
                 return True
                 
     return False
