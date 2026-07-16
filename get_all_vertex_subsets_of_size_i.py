43ee2de0a6a30dc368fc4d68de86015eb1da0f22
Agastya <aarora377@gatech.edu>
2026-07-16 19:44:32 +0530

    made it such that 0 can be included in the random vertex subset that we get (easier to deal with matrices now)

--- get_all_vertex_subsets_of_size_i.py
+++ get_all_vertex_subsets_of_size_i.py
@@ -1,3 +1,4 @@
 from itertools import combinations
 def get_all_vertex_subsets_of_size_i(n,i): #n=no. of vertices, i=size of the vertex-subset we are looking at
-    subset_list=list(combinations(range(1,n+1),i))
+    subset_list=list(combinations(range(0,n),i))
+    return subset_list
--- get_minor.py
+++ get_minor.py
@@ -1,12 +1,12 @@
 import numpy as np
-def get_minor(A):
+def get_minor(A,i):#the size of vertex subset we are looking at
     newA=np.array(A) #array instead of nested list
-    for i in lst: #list is all combinations
-        VertexSubset=[1] # VertexSubset is just one combination
-        for j in VertexSubset:
-            Index=j-1
-            matrix1=np.delete(newA,Index,axis=0)
-            matrix2=np.delete(matrix1,Index,axis=1)
+    lst=get_all_vertex_subsets_of_size_i(i)
+    for i in lst: #lst is the list of all combinations
+        VertexSubset=i # VertexSubset is just one combination
+        for index in VertexSubset:
+            matrix1=np.delete(newA,index,axis=0)
+            matrix2=np.delete(matrix1,index,axis=1)
             newA=matrix2
     return newA
 
