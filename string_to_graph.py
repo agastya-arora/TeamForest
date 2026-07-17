06a48de958f39fef04f85addcc52c7f84aa75dbc
CoraShaklee <cshaklee06@gmail.com>
2026-07-17 06:59:38 -0400

    Replace string generation function with list generator

--- string_to_graph.py
+++ string_to_graph.py
@@ -1,4 +1,4 @@
-import generate_string as gs
+import generate_list as gs
 
 def create_matrix(vertices,string):
     print(string)
@@ -15,40 +15,15 @@ def create_matrix(vertices,string):
             CountStringItem+=1
             CountColumn+=1
 
-<<<<<<< HEAD
-count2 = 1
-for x in range(n):
-    count1 = n
-    for z in G[x][n:]:
-        G[x][count1]=my_string[count2]
-        count2+=1
-        count1+=1
-
-for i in range(n):
-    for j in range(n+m):
-        G[j][i]=G[i][j]
-for x in G:
-    print(x)
-
-'''count3 = 1
-for x in range(t):
-    count1=m
-    for z in G[n:]:
-        G[count1][x]=my_string[count3]
-        count3+=1
-        count1+=1'''
-=======
     for i in range(n):
         for j in range(vertices):
             #print(G[i][j])
             G[j][i]=G[i][j]
     return G
 
-vertices = int(input('Number of Vertices: '))
-for x in create_matrix(vertices,gs.generate_string(vertices)):
-    print(x)
-
->>>>>>> aca03b1ea7a9267a9151c78083fed2816c5bbe1f
+'''vertices = int(input('Number of Vertices: '))
+for x in create_matrix(vertices,gs.generate_list(vertices)):
+    print(x)'''
 
 
 
