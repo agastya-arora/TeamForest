274779dd4bcef04d2f55b8f5b8ca09fb057b38c9
agastya-arora <aarora377@gatech.edu>
2026-07-20 05:44:38 -0700

    added matrix_to_list code here for consistency

--- adjMatrix_to_adjList.py
+++ adjMatrix_to_adjList.py
@@ -12,6 +12,20 @@ def adjMatrix_to_adjList(matrix):
                 A[i]+=[count]
     return A
 
+def matrix_to_list(adj_matrix):
+    n = len(adj_matrix)
+    
+    # init empty list
+    adj_list = [[] for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            #Append only the neighbor
+            if adj_matrix[i][j] != 0:
+                adj_list[i].append(j)
+                
+    return adj_list
+
 A = [
     [0, 1, 1, 0, 0],
     [1, 0, 1, 1, 0],
