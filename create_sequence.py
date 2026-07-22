99486d3dd9277f542c2884072ea5474884000ff6
CoraShaklee <cshaklee06@gmail.com>
2026-07-22 06:54:36 -0400

    Add files via upload

---
+++ create_sequence.py
@@ -0,0 +1,13 @@
+import find_cycles as fc
+import get_all_minors
+
+def create_sequence(vertices,matrix):
+    for y in range(2,vertices):
+        all_minors = get_all_minors.get_minor(matrix,vertices,y)
+        NumCollections = 0 #Counts number of valid sub forests for y vertices
+        for minor in all_minors:
+            cycle_status = fc.find_cycle(minor)
+            if not cycle_status:
+                NumCollections += 1 #Adds if no cycle is found
+        list.append(NumCollections) #creates list of number of forests for i-1 to i+1
+    return list
