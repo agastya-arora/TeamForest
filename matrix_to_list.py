369ef3b233fbf758bfb56d3a6ce2c7c7b4026299
agastya-arora <aarora377@gatech.edu>
2026-07-23 06:04:56 -0700

    rename create_string_rep.py to matrix_to_list.py for ease of understanding

--- matrix_to_list.py
+++ matrix_to_list.py
@@ -1,15 +1,15 @@
-def create_string_rep(n,G):
-    string_rep = [n]
+def matrix_to_list(n,G):
+    list_rep = [n]
     count1=1
     for row in G:
         count2=1
         if count1<=n:
             for y in row:
                 if count2>n:
-                    string_rep.append(y)
+                    list_rep.append(y)
                 count2+=1
         count1+=1
-    return string_rep
+    return list_rep
 
 G = [[0,0,1,1,1],
      [0,0,0,1,1],
@@ -18,4 +18,4 @@ G = [[0,0,1,1,1],
      [1,1,0,0,0]]
 n=2
 #input size of matrix
-print(create_string_rep(n,G))
+print(matrix_to_list(n,G))
