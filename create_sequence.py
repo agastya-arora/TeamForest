b80583e56925dcb3d8f1e444f07c5f9b0972cf66
Rendi Adinata <144026024+xinuzo@users.noreply.github.com>
2026-07-24 17:44:44 +0700

    the function just take matrix input

--- create_sequence.py
+++ create_sequence.py
@@ -1,13 +1,15 @@
 import find_cycles as fc
-import get_all_minors
+import get_all_minors as gam
 
-def create_sequence(vertices,matrix):
-    for y in range(1,vertices):
-        all_minors = get_all_minors.get_minor(matrix,vertices,y)
+def create_sequence(matrix):
+    lst=[]
+    vertices=len(matrix[0])
+    for y in range(1,vertices+1):
+        all_minors = gam.get_minor(matrix,vertices,y)
         NumCollections = 0 #Counts number of valid sub forests for y vertices
         for minor in all_minors:
             cycle_status = fc.find_cycle(minor)
             if not cycle_status:
                 NumCollections += 1 #Adds if no cycle is found
-        list.append(NumCollections) #creates list of number of forests for i-1 to i+1
-    return list
+        lst.append(NumCollections) #creates list of number of forests for i-1 to i+1
+    return lst
